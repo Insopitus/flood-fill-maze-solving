e243ba2e1fb0039848d6584870c180c301dd3c0b
Insopitus <jessefor@hotmail.com>
2023-03-16 00:17:42 +0800

    parse wip

--- src/lib.rs
+++ src/lib.rs
@@ -22,116 +22,116 @@ pub struct Maze {
 // origin is left top
 impl Maze {
     pub fn flood_fill(&self) {
-        let mut stack = vec![];
-        let start = &self.tiles[self.goal_position];
-        start.value.set(Some(0));
-        stack.push(start);
-        while let Some(tile) = stack.pop() {
-            let pos = tile.position;
-            let pos_x = pos % self.width;
-            let pos_y = pos / self.width;
-            if pos_x < self.width - 1 && tile.right {
-                let right = &self.tiles[pos + 1];
-                if let Some(value) = right.value.get() {
-                    if value + 1 < tile.value.get().unwrap() {
-                        tile.value.set(Some(value + 1));
-                        stack.push(tile);
-                    }
-                } else {
-                    right.value.set(Some(tile.value.get().unwrap() + 1));
-                    stack.push(right);
-                }
-            };
-            if pos_x > 0 && tile.left {
-                let left = &self.tiles[pos - 1];
-                if let Some(value) = left.value.get() {
-                    if value + 1 < tile.value.get().unwrap() {
-                        tile.value.set(Some(value + 1));
-                        stack.push(tile);
-                    }
-                } else {
-                    left.value.set(Some(tile.value.get().unwrap() + 1));
-                    stack.push(left);
-                }
-            };
-            if pos_y < self.width - 1 && tile.lower {
-                let lower = &self.tiles[pos + self.width];
-                if let Some(value) = lower.value.get() {
-                    if value + 1 < tile.value.get().unwrap() {
-                        tile.value.set(Some(value + 1));
-                        stack.push(tile);
-                    }
-                } else {
-                    lower.value.set(Some(tile.value.get().unwrap() + 1));
-                    stack.push(lower);
-                }
-            };
-            if pos_y > 0 && tile.upper {
-                let upper = &self.tiles[pos - self.width];
-                if let Some(value) = upper.value.get() {
-                    if value + 1 < tile.value.get().unwrap() {
-                        tile.value.set(Some(value + 1));
-                        stack.push(tile);
-                    }
-                } else {
-                    upper.value.set(Some(tile.value.get().unwrap() + 1));
-                    stack.push(upper);
-                }
-            };
-        }
+        // let mut stack = vec![];
+        // let start = &self.tiles[self.goal_position];
+        // start.value.set(Some(0));
+        // stack.push(start);
+        // while let Some(tile) = stack.pop() {
+        //     let pos = tile.position;
+        //     let pos_x = pos % self.width;
+        //     let pos_y = pos / self.width;
+        //     if pos_x < self.width - 1 && tile.right {
+        //         let right = &self.tiles[pos + 1];
+        //         if let Some(value) = right.value.get() {
+        //             if value + 1 < tile.value.get().unwrap() {
+        //                 tile.value.set(Some(value + 1));
+        //                 stack.push(tile);
+        //             }
+        //         } else {
+        //             right.value.set(Some(tile.value.get().unwrap() + 1));
+        //             stack.push(right);
+        //         }
+        //     };
+        //     if pos_x > 0 && tile.left {
+        //         let left = &self.tiles[pos - 1];
+        //         if let Some(value) = left.value.get() {
+        //             if value + 1 < tile.value.get().unwrap() {
+        //                 tile.value.set(Some(value + 1));
+        //                 stack.push(tile);
+        //             }
+        //         } else {
+        //             left.value.set(Some(tile.value.get().unwrap() + 1));
+        //             stack.push(left);
+        //         }
+        //     };
+        //     if pos_y < self.width - 1 && tile.lower {
+        //         let lower = &self.tiles[pos + self.width];
+        //         if let Some(value) = lower.value.get() {
+        //             if value + 1 < tile.value.get().unwrap() {
+        //                 tile.value.set(Some(value + 1));
+        //                 stack.push(tile);
+        //             }
+        //         } else {
+        //             lower.value.set(Some(tile.value.get().unwrap() + 1));
+        //             stack.push(lower);
+        //         }
+        //     };
+        //     if pos_y > 0 && tile.upper {
+        //         let upper = &self.tiles[pos - self.width];
+        //         if let Some(value) = upper.value.get() {
+        //             if value + 1 < tile.value.get().unwrap() {
+        //                 tile.value.set(Some(value + 1));
+        //                 stack.push(tile);
+        //             }
+        //         } else {
+        //             upper.value.set(Some(tile.value.get().unwrap() + 1));
+        //             stack.push(upper);
+        //         }
+        //     };
+        // }
     }
 
     pub fn solve(&self) -> Vec<Index> {
-        // todo!();
-        let mut solution = Vec::new();
-        solution.push(self.start_position);
-        let mut tile = &self.tiles[self.start_position];
-        while tile.position != self.goal_position {
-            let pos = tile.position;
-            let pos_x = pos % self.width;
-            let pos_y = pos / self.width;
-            if pos_x < self.width - 1 && tile.right {
-                let right = &self.tiles[pos + 1];
-                if let Some(value) = right.value.get() {
-                    if value + 1 == tile.value.get().unwrap() {
-                        solution.push(pos + 1);
-                        tile = right;
-                        continue;
-                    }
-                }
-            };
-            if pos_x > 0 && tile.left {
-                let left = &self.tiles[pos - 1];
-                if let Some(value) = left.value.get() {
-                    if value + 1 == tile.value.get().unwrap() {
-                        solution.push(pos - 1);
-                        tile = left;
-                        continue;
-                    }
-                }
-            };
-            if pos_y < self.width - 1 && tile.lower {
-                let lower = &self.tiles[pos + self.width];
-                if let Some(value) = lower.value.get() {
-                    if value + 1 == tile.value.get().unwrap() {
-                        solution.push(pos + self.width);
-                        tile = lower;
-                        continue;
-                    }
-                }
-            };
-            if pos_y > 0 && tile.upper {
-                let upper = &self.tiles[pos - self.width];
-                if let Some(value) = upper.value.get() {
-                    if value + 1 == tile.value.get().unwrap() {
-                        solution.push(pos - self.width);
-                        tile = upper;
-                        continue;
-                    }
-                }
-            };
-        }
-        solution
+        todo!();
+        // let mut solution = Vec::new();
+        // solution.push(self.start_position);
+        // let mut tile = &self.tiles[self.start_position];
+        // while tile.position != self.goal_position {
+        //     let pos = tile.position;
+        //     let pos_x = pos % self.width;
+        //     let pos_y = pos / self.width;
+        //     if pos_x < self.width - 1 && tile.right {
+        //         let right = &self.tiles[pos + 1];
+        //         if let Some(value) = right.value.get() {
+        //             if value + 1 == tile.value.get().unwrap() {
+        //                 solution.push(pos + 1);
+        //                 tile = right;
+        //                 continue;
+        //             }
+        //         }
+        //     };
+        //     if pos_x > 0 && tile.left {
+        //         let left = &self.tiles[pos - 1];
+        //         if let Some(value) = left.value.get() {
+        //             if value + 1 == tile.value.get().unwrap() {
+        //                 solution.push(pos - 1);
+        //                 tile = left;
+        //                 continue;
+        //             }
+        //         }
+        //     };
+        //     if pos_y < self.width - 1 && tile.lower {
+        //         let lower = &self.tiles[pos + self.width];
+        //         if let Some(value) = lower.value.get() {
+        //             if value + 1 == tile.value.get().unwrap() {
+        //                 solution.push(pos + self.width);
+        //                 tile = lower;
+        //                 continue;
+        //             }
+        //         }
+        //     };
+        //     if pos_y > 0 && tile.upper {
+        //         let upper = &self.tiles[pos - self.width];
+        //         if let Some(value) = upper.value.get() {
+        //             if value + 1 == tile.value.get().unwrap() {
+        //                 solution.push(pos - self.width);
+        //                 tile = upper;
+        //                 continue;
+        //             }
+        //         }
+        //     };
+        // }
+        // solution
     }
     // get neighboring tiles of a certain on (top, down, left, right), if there's the border of the maze on that direction, returns None
     pub fn get_neighbors(
@@ -154,17 +154,49 @@ impl Maze {
 
 
 #[derive(Debug)]
-enum MazeParseError{
-    InvalidToken(char),
+pub enum MazeParseError{
+    InvalidToken,
     InvalidSize
 }
 impl FromStr for Maze{
     type Err = MazeParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        
+        let chars:Vec<_> = s.chars().collect();
+        let mut start:usize = 0;
+        let mut tokens:Vec<&[char]> = vec![];
+        for (i,c) in chars.iter().enumerate(){
+            if (chars[start].is_alphabetic() && c.is_numeric() )|| (chars[start].is_numeric() && c.is_alphabetic()) {
+                tokens.push(&chars[start..i]);
+                start = i;
+            }
+        }
+        dbg!(&tokens);
+        let mut tokens = tokens.iter();
+        let w_char :String = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect();
+        if w_char!="w" {return Err(MazeParseError::InvalidToken)}
+        let w_num:usize = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect::<String>().parse().map_err(|_|MazeParseError::InvalidToken)?;
+        let h_char :String = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect();
+        if h_char!="h" {return Err(MazeParseError::InvalidToken)}
+        let h_num:usize = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect::<String>().parse().map_err(|_|MazeParseError::InvalidToken)?;
+        let s_char :String = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect();
+        if s_char!="s" {return Err(MazeParseError::InvalidToken)}
+        let s_num:usize = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect::<String>().parse().map_err(|_|MazeParseError::InvalidToken)?;
+        let g_char :String = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect();
+        if g_char!="g" {return Err(MazeParseError::InvalidToken)}
+        let g_num:usize = tokens.next().ok_or_else(||MazeParseError::InvalidToken)?.iter().collect::<String>().parse().map_err(|_|MazeParseError::InvalidToken)?;
+
+        Ok(Self{
+            width:w_num,
+            height:h_num,
+            start_position:s_num,
+            goal_position:g_num,
+            values:vec![],
+            tiles:vec![]
+        })
+       
     }
 }
-enum Tile {
+pub enum Tile {
     Wall,
     Path,
 }
@@ -172,8 +204,18 @@ enum Tile {
 #[cfg(test)]
 mod test {
 
+    use std::str::FromStr;
+
     use crate::{Maze, Tile};
 
     #[test]
     fn basic() {}
+    #[test]
+    fn parse(){
+        let maze = Maze::from_str("w5h5s10g0#0000011101000011010100100").unwrap();
+        assert_eq!(maze.width, 5);
+        assert_eq!(maze.height, 5);
+        assert_eq!(maze.start_position, 10);
+        assert_eq!(maze.goal_position, 0);  
+    }
 }
